5477e13ee648bcbfc4628289f8c334eab4d6ee3b
Allan Wazacz <cazwazacz@gmail.com>
2018-07-21 16:13:15 +0100

    Update test for FilesPane

--- test/app/js/filesPane.spec.js
+++ test/app/js/filesPane.spec.js
@@ -2,17 +2,19 @@ const expect = require('chai').expect;
 const FilesPane = require('../../../app/js/filesPane');
 
 describe('filesPane', () => {
-    let filesPane, document;
+    let filesPane, document, filesDiv;
 
     beforeEach(() => {
-        document = { getElementById: () => {} }
+        filesDiv = { innerHTML: 'hello' }
+        document = { getElementById: () => { return filesDiv } }
 
         filesPane = new FilesPane(document);
     })
-    
-    describe('#_createFileDiv', () => {
-        it('returns the correct string', () => {
-            expect(filesPane._createFileDiv('hello')).to.eq('<div class="filename">hello</div>')
+
+    describe('#addFileToDiv', () => {
+        it('adds the file div to the files div', () => {
+            filesPane.addFileToDiv('some filename')
+            expect(filesDiv.innerHTML).to.eq('hello<div class="filename">some filename</div>')
         })
     })
 });
